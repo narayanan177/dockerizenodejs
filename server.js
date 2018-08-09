3f762a7678f8d2380e33b031d83caafb02680a73
Sathya <narayanan177@rediffmail.com>
2018-08-09 20:10:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello World, Welcome To Sathya''s Kubernetes Learning Tutorials \n');
 });
 
 app.listen(PORT, HOST);
