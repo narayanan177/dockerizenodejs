ae111fcbc6ab260a0b75e370897a2ac2e4b17465
Sathya <narayanan177@rediffmail.com>
2018-08-30 10:51:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Welcome To Home \n');
+  res.send('Hello World, Welcome To CAPE Overview session \n');
 });
 
 app.listen(PORT, HOST);
